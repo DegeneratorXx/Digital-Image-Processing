52ffe4edcef790a1b99b9c95b2aba972d5b15137
Lakshit Khandelwal <130062008+DegeneratorXx@users.noreply.github.com>
2025-01-16 15:10:47 +0530

    Add files via upload

---
+++ histogram equalization lab2/lab2_basics.m
@@ -0,0 +1,40 @@
+% Generate a 1x5 array with random integers between 0 and 6
+array = round(6 * rand(1, 5));
+array 
+
+% Sorting the array using the built-in sort function
+sort_matrix = sort(array);
+
+% Cumulative sum of the sorted array
+sortedsum = cumsum(sort_matrix);
+
+% Custom bubble sort implementation
+n = length(array);
+
+for i = 1:n-1
+    for j = 1:n-i
+        if array(j) > array(j+1)
+            % Swap the elements
+            temp = array(j);
+            array(j) = array(j+1);
+            array(j+1) = temp;
+        end
+    end
+end
+
+% Display the final sorted array after bubble sort
+disp('custom bubble sort:');
+disp(array);
+
+% Display the cumulative sum of the original and sorted arrays
+disp('Cumulative sum of the original array:');
+disp(b);
+
+disp('Cumulative sum of the sorted array:');
+sortedsum
+
+unique_element=unique(array);
+sz=length(unique_element);;
+
+disp('Number of unique elemenets:')
+sz
